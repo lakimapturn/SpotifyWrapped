9a9f0a2db6879de000b358a1de8281a125486e14
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-04-07 15:47:42 -0400

    added spotify token to user object

--- app/src/main/java/com/example/spotifywrapped/Models/User.java
+++ app/src/main/java/com/example/spotifywrapped/Models/User.java
@@ -5,35 +5,52 @@ import java.util.ArrayList;
 public class User {
     private String username;
     private String password;
+    private String spotifyToken;
     private SpotifyWrapped spotifyWrapped;
     private ArrayList<User> following;
 
+    public User(String username, String password, String spotifyToken, SpotifyWrapped spotifyWrapped, ArrayList<User> following) {
+        this.username = username;
+        this.password = password;
+        this.spotifyToken = spotifyToken;
+        this.spotifyWrapped = spotifyWrapped;
+        this.following = following;
+    }
+
     public String getUsername() {
         return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public SpotifyWrapped getSpotifyWrapped() {
-        return spotifyWrapped;
-    }
-
-    public ArrayList<User> getFollowing() {
-        return following;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
+    public String getSpotifyToken() {
+        return spotifyToken;
+    }
+
+    public void setSpotifyToken(String spotifyToken) {
+        this.spotifyToken = spotifyToken;
+    }
+
+    public SpotifyWrapped getSpotifyWrapped() {
+        return spotifyWrapped;
+    }
+
     public void setSpotifyWrapped(SpotifyWrapped spotifyWrapped) {
         this.spotifyWrapped = spotifyWrapped;
+    }
+
+    public ArrayList<User> getFollowing() {
+        return following;
     }
 
     public void setFollowing(ArrayList<User> following) {
