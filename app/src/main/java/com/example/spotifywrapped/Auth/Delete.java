b8d6e42220aa7484d241d2d3d90cdd85ee2df3ab
Advait Sepuri <advaitsepuri@gmail.com>
2024-04-11 23:48:50 -0400

    delete account functionality

--- app/src/main/java/com/example/spotifywrapped/Auth/Delete.java
+++ app/src/main/java/com/example/spotifywrapped/Auth/Delete.java
@@ -9,7 +9,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class Delete {
 //    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 //
-//user.delete()
+//     user.delete()
 //        .addOnCompleteListener(new OnCompleteListener<Void>() {
 //        @Override
 //        public void onComplete(@NonNull Task<Void> task) {
--- app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
+++ app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
@@ -8,11 +8,41 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.spotifywrapped.databinding.DeleteAccountBinding;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class DeleteAccount extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.delete_account, container, false);
+
+    private DeleteAccountBinding binding;
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.delete_account, container, false);
+//    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DeleteAccountBinding.inflate(inflater, container, false);
+        binding.deleteAccountBtn.setOnClickListener(v -> deleteAccountForever());
+        return binding.getRoot();
+    }
+
+    private void deleteAccountForever() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("DeleteAccount", "User account deleted.");
+                        }
+                    }
+                });
     }
 }
