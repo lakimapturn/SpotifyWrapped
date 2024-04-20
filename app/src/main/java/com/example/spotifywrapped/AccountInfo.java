3ed10b573c0c56629a46d83effd242b3bd86c817
Advait Sepuri <advaitsepuri@gmail.com>
2024-04-20 15:07:31 -0400

    Email Display

--- app/src/main/java/com/example/spotifywrapped/AccountInfo.java
+++ app/src/main/java/com/example/spotifywrapped/AccountInfo.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.navigation.fragment.NavHostFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import com.example.spotifywrapped.databinding.UserAccountInfoBinding;
 
 public class AccountInfo extends Fragment {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = mAuth.getCurrentUser();
     private UserAccountInfoBinding binding;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,8 +27,18 @@ public class AccountInfo extends Fragment {
         binding.arrowForward1.setOnClickListener(v -> onArrow1Clicked());
         binding.arrowForward2.setOnClickListener(v -> onArrow2Clicked());
         binding.arrowForward3.setOnClickListener(v -> onArrow3Clicked());
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String email = currentUser.getEmail();
+            TextView emailTextView = binding.email;
+            emailTextView.setText(email);
+        }
+
         return binding.getRoot();
     }
+
+
 
     private void onArrow1Clicked() {
         NavHostFragment.findNavController(this).navigate(R.id.pastWrapped);
